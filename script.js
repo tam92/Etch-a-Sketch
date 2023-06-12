882a53414cd8e207aefed51ea7350f3961affda9
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-12 22:35:43 +0200

    generates a new customized grid

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
 let container = document.getElementById('container');
 
+const btn = document.createElement('button');
+btn.textContent = "New grid!";
+btn.classList.add('button');
+btn.addEventListener('click', (e) => {
+    let sideSquares;
+    
+    do {
+        sideSquares = prompt("How many squares per side do you want? (max is 100)");
+    } while (isNaN(parseInt(sideSquares)) || parseInt(sideSquares) > 100);
+
+    console.log(sideSquares);
+    createGrid(sideSquares);
+});
+
+document.getElementsByTagName('body')[0].prepend(btn);
+
 // Create 16x16 divs (256)
 
 for (let i = 0; i < 256; i++) {
@@ -8,8 +24,29 @@ for (let i = 0; i < 256; i++) {
     newDiv.classList.add('square');
     const hoveredSquare = 
     newDiv.addEventListener('mouseover', (e) => {
-        console.log("mouse");
         newDiv.classList.add('hover');
     } );
     container.appendChild(newDiv);
+}
+
+function createGrid(sideSquares) {
+    let children = document.getElementById('container');
+
+    // removes the previous grid from container
+    while (children.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+960 % sideSquares
+    let total = sideSquares * sideSquares;
+    // new grid
+    for (let i = 0; i < total; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('square');
+        newDiv.style.flexBasis = `${960/sideSquares}px`;
+        const hoveredSquare = 
+        newDiv.addEventListener('mouseover', (e) => {
+            newDiv.classList.add('hover');
+        } );
+        container.appendChild(newDiv);
+    }
 }
